fd5d3540d429f1eb20f2cf1fabf1f51b51992435
ManalFadil <ma_fadil@etu.enset-media.ac.ma>
2026-01-13 10:39:48 +0100

    feat: improve preprocessing pipeline

--- src/features.py
+++ src/features.py
@@ -2,8 +2,17 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+#def build_numeric_preprocess():
+    #return Pipeline(steps=[
+#("imputer", SimpleImputer(strategy="median")),
+#("scaler", StandardScaler()),
+#])
+from sklearn.preprocessing import FunctionTransformer
+def _clip(X):
+    return X.clip(-3, 3)
 def build_numeric_preprocess():
     return Pipeline(steps=[
-("imputer", SimpleImputer(strategy="median")),
-("scaler", StandardScaler()),
-])
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler()),
+        ("clip", FunctionTransformer(_clip)),
+    ])
